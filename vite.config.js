02a6078d0e5a143bf7582ac0172dcd1dc16e9b73
aryaman404 <aryamansivam@gmail.com>
2025-03-03 11:24:57 +0530

    updated viteConfig for netlify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/Contact_Management/",
+  base: "/", // Ensure base is set correctly
 });
